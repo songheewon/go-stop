bae34147bed5be3348e736c9c59fe65b88496481
원송희 <songheew1020@gmail.com>
2022-04-26 21:21:50 +0900

    Update TurnDecision.py

--- GoStop/TurnDecision.py
+++ GoStop/TurnDecision.py
@@ -10,11 +10,6 @@ class GoStop(object):
 
 
 class TurnDecision(Turn):  # 턴 마지막 상태 (3단계)
-    def get_left_cards(self): # 덱에 남은 카드 수를 리턴
-        if len(self.deck) > 0:
-            return len(self.deck)
-        return 0
-
     def after_decision(self, action):
         if action == GoStop.GO: # GO를 선택했다면
             current = TurnStart(prev_turn=self) # 다시 턴 변경
